cc01238e59d9fcb68070545ae6594ed871d62df0
Javier Lanatta <jlanatta@quadiontech.com>
2016-01-28 10:54:35 -0300

    Initial version

--- arduino/SousVide/SousVide.ino
+++ arduino/SousVide/SousVide.ino
@@ -1,17 +1,21 @@
-#define PIN_KEYBOARD_UP 9
-#define PIN_KEYBOARD_DOWN 8
-//#define PIN_KEYBOARD_LEFT 12
-//#define PIN_KEYBOARD_RIGHT 13
+#define REQUIRESALARMS true
 
-
-#include <U8glib.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ssd1306syp.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI 
-
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
+#define SDA_PIN 8
+#define SCL_PIN 9
+#define PIN_KEYBOARD_UP 11
+#define PIN_KEYBOARD_DOWN 12
+#define PIN_KEYBOARD_ON 13
+#define PIN_HEATER 3
+#define PIN_PUMP 4
+
+Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
  
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
@@ -20,70 +24,101 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-int currentStep = 0;
+float currentTemp;
+float targetTemp;
+bool heaterOn = false;
+bool isOn = false;
 
-void setup(void) {
-  
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-    u8g.setColorIndex(255);     // white
-  }
-  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
-    u8g.setColorIndex(3);         // max intensity
-  }
-  else if ( u8g.getMode() == U8G_MODE_BW ) {
-    u8g.setColorIndex(1);         // pixel on
-  }
-  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-    u8g.setHiColorByRGB(255,255,255);
-  }
-  
+void setup()
+{
+  targetTemp = 55;
   sensors.begin();
+  display.initialize();
+  pinMode(PIN_KEYBOARD_UP, INPUT_PULLUP);
+  pinMode(PIN_KEYBOARD_DOWN, INPUT_PULLUP);
+  pinMode(PIN_KEYBOARD_ON, INPUT_PULLUP);
+  pinMode(PIN_HEATER, OUTPUT);
+  pinMode(PIN_PUMP, OUTPUT);
 }
 
-void loop(void) {
-  sensors.requestTemperatures(); // Send the command to get temperatures
+void loop() {
+  updateTargetTemp();
+  updateCurrentTemp();
+  updateHeaterAndPump();
+  updateDisplay();
+}
 
-  // picture loop
-  u8g.firstPage();  
-  do {
-    draw();
-  } while( u8g.nextPage() );
+void updateTargetTemp() {
+  int pinUp = digitalRead(PIN_KEYBOARD_UP);
+  int pinDown = digitalRead(PIN_KEYBOARD_DOWN);
+  int pinOn = digitalRead(PIN_KEYBOARD_ON);
   
-  // rebuild the picture after some delay
-  delay(500);
-}
-
-float tempToSet = 55.0;
-int lastKey = 0;
-
-int readKey() {
-  bool keys[4] = {digitalRead(PIN_KEYBOARD_UP), digitalRead(PIN_KEYBOARD_DOWN)};  
-  for (int i=0;i<2;i++) {
-    if (keys[i] == LOW) {
-      if (lastKey == i+1) {
-        return 0;
-      } else {
-        lastKey = i+1;
-        return lastKey;
-      }
-    }
+  if (pinUp == LOW) {
+    targetTemp ++;
   }
-  lastKey = 0;
-  return 0;
+  if (pinDown == LOW) {
+    targetTemp --;
+  }  
+  if (pinOn == LOW) {
+    isOn = !isOn;
+  }
 }
 
-void drawTemp(void) {
-  // graphic commands to redraw the complete screen should be placed here  
-  u8g.setFont(u8g_font_fur25);
-  //u8g.setFont(u8g_font_osb21);
+int targetTempLastUpdated = 0;
 
-  float temp = sensors.getTempCByIndex(0);
-  char temperature[10];
-  char str_temp[6];
+void updateCurrentTemp() {
+  if (millis() - targetTempLastUpdated > 500) {
+    sensors.requestTemperaturesByIndex(0); // Send the command to get temperatures
+    currentTemp = sensors.getTempCByIndex(0);
+    heaterOn = isOn && currentTemp < targetTemp;  
+    targetTempLastUpdated = millis();
+  }
+}
 
-  /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
-  dtostrf(temp, 4, 2, str_temp);
-  sprintf(temperature,"%s C", str_temp);
-  u8g.drawStr(0, 45, temperature);
+void updateHeaterAndPump() {
+  if (isOn) {
+    digitalWrite(PIN_PUMP, LOW);
+    if (heaterOn) {
+      digitalWrite(PIN_HEATER, LOW);
+    } else {
+      digitalWrite(PIN_HEATER, HIGH);    
+    }
+  } else {
+    digitalWrite(PIN_HEATER, HIGH);
+    digitalWrite(PIN_PUMP, HIGH);
+  }
+}
+
+int displayLastUpdated = 0;
+
+void updateDisplay() {
+  if (millis() - displayLastUpdated > 150) {
+    display.clear();
+    
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.print(targetTemp);
+    if (isOn) {
+      display.print(" ON");
+    } else {
+      display.print(" OFF");
+    }
+    
+    display.fillRoundRect(0, 20, 128, 40, 8, WHITE);
+    display.setTextSize(4);
+    display.setTextColor(BLACK);
+    display.setCursor(6, 26);
+    display.print(currentTemp);
+    
+    if (heaterOn) {
+      display.fillCircle(120, 7, 7, WHITE);  
+    } else {
+      display.drawCircle(120, 7, 7, WHITE);      
+    }
+    
+    displayLastUpdated = millis();
+    display.update();
+  }
 }
 
